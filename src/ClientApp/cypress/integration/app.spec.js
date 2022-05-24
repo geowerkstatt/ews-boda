c9c87d7a1cffa8f8074f22fbd7d660813ba3ba00
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2022-05-24 17:55:26 +0200

    Configure linter for test files

--- src/ClientApp/cypress/integration/app.spec.js
+++ src/ClientApp/cypress/integration/app.spec.js
@@ -1,9 +1,9 @@
-describe('General app tests', () => {
-  it('Displays the application version', () => {
-    const expectedVersion = '457.31.0.4651'
-    cy.intercept('/version', expectedVersion)
+describe("General app tests", () => {
+  it("Displays the application version", () => {
+    const expectedVersion = "457.31.0.4651";
+    cy.intercept("/version", expectedVersion);
 
-    cy.visit('/')
-    cy.contains('Version: ' + expectedVersion)
-  })
-})
+    cy.visit("/");
+    cy.contains("Version: " + expectedVersion);
+  });
+});
