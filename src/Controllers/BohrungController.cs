80b9fea9390f9d613dd9385a2c6fc9068c8ebdb6
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-06-20 13:37:21 +0200

    Improve Bohrungen Controller and add Tests

--- src/Controllers/BohrungController.cs
+++ src/Controllers/BohrungController.cs
@@ -16,9 +16,9 @@ public class BohrungController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<BohrungDTO>>> Get()
+    public async Task<ActionResult<IEnumerable<BohrungDTO>>> GetAsync()
     {
-        return await context.Bohrungen.Take(20).Select(x => BohrungToDTO(x)).ToListAsync().ConfigureAwait(false);
+        return await context.Bohrungen.Select(x => BohrungToDTO(x)).ToListAsync().ConfigureAwait(false);
     }
 
     private static BohrungDTO BohrungToDTO(Bohrung bohrung)
@@ -28,6 +28,19 @@ public class BohrungController : ControllerBase
             Id = bohrung.Id,
             StandortId = bohrung.StandortId,
             Bezeichnung = bohrung.Bezeichnung,
+            Bemerkung = bohrung.Bemerkung,
+            Ablenkung = bohrung.Ablenkung,
+            Datum = bohrung.Datum,
+            DurchmesserBohrloch = bohrung.DurchmesserBohrloch,
+            Erstellungsdatum = bohrung.Erstellungsdatum,
+            UserErstellung = bohrung.UserErstellung,
+            HAblenkung = bohrung.HAblenkung,
+            HQualitaet = bohrung.HQualitaet,
+            Mutationsdatum = bohrung.Mutationsdatum,
+            Qualitaet = bohrung.Qualitaet,
+            QualitaetBemerkung = bohrung.QualitaetBemerkung,
+            QuelleRef = bohrung.QuelleRef,
+            UserMutation = bohrung.UserMutation,
             Geometrie = new Point((int)bohrung.Geometrie.X, (int)bohrung.Geometrie.Y),
         };
     }
---
+++ tests/BohrungControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWS;
+
+[TestClass]
+public class BohrungControllerTest
+{
+    [TestMethod]
+    public async Task GetAsync()
+    {
+        using var context = new EwsContext(new DbContextOptionsBuilder<EwsContext>()
+            .UseNpgsql("Host=localhost; Username=postgres;Password=VINTAGEMAGIC;Database=ews", option => option.UseNetTopologySuite()).Options);
+        var controller = new BohrungController(context);
+        var result = await controller.GetAsync().ConfigureAwait(false);
+        var bohrungen = result.Value;
+        Assert.AreEqual(8000, bohrungen?.Count());
+        Assert.AreEqual("Reverse-engineered web-enabled emulation", bohrungen?.First().Bemerkung);
+        Assert.AreEqual(27330, bohrungen?.Last().DurchmesserBohrloch);
+        Assert.AreEqual(new Point(2603642, 1240821), bohrungen?.First().Geometrie);
+        Assert.AreEqual(new DateTime(2022, 4, 29, 0, 0, 0), bohrungen?.Last().Datum);
+    }
+}
