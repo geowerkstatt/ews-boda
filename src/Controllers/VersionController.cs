055bb88bffb4dfb0eec37cafd0b3633681e4ef50
danjov <daniel.jovanovic@geowerkstatt.ch>
2022-06-21 18:15:31 +0200

    Use InformationalVersion as version information

--- src/Controllers/VersionController.cs
+++ src/Controllers/VersionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace EWS;
 
@@ -7,5 +8,9 @@ namespace EWS;
 public class VersionController : ControllerBase
 {
     [HttpGet]
-    public string Get() => "0.1.0";
+    public string Get()
+    {
+        var assembly = typeof(Program).Assembly;
+        return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? assembly.GetName().Version.ToString();
+    }
 }
