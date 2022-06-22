e34a11c5af22aa77ebe3d2e0cc4d72f174d63ef4
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-06-22 16:31:07 +0200

    Fix frontend tests

--- src/ClientApp/cypress/integration/home.spec.js
+++ src/ClientApp/cypress/integration/home.spec.js
@@ -1,5 +1,14 @@
+import standorteGemeinde from "../fixtures/standorteGemeinde.json";
+import standorteGbnummer from "../fixtures/standorteGbnummer.json";
+import standorteBezeichnung from "../fixtures/standorteBezeichnung.json";
+
 describe("Home page tests", () => {
   it("Show search result box if search for gemeinde", function () {
+    cy.intercept(
+      "/standort?gemeindenummer=2521&gbnummer=&bezeichnung=&erstellungsdatum=&mutationsdatum=",
+      standorteGemeinde
+    );
+
     cy.visit("/");
     cy.get("div[name=home-container]").should("not.contain", "Suchresultate");
 
@@ -7,27 +16,33 @@ describe("Home page tests", () => {
     cy.get("div[name=gemeinde] input").should("be.visible").click({ force: true }).type("Hein{downarrow}{enter}");
     cy.get("button[name=submit-button]").should("be.visible").click();
     cy.get("div[name=home-container]").should("contain", "Suchresultate");
-    cy.get("tbody").children().should("have.length", 27);
+    cy.get("tbody").children().should("have.length", 4);
   });
 
   it("Show search result box if search for grundbuchnummer", function () {
+    const gbnummer = "h5r0wdwsz6ef39zb2d31a0zfou7i4tdguvddcklb";
+    cy.intercept(
+      `/standort?gemeindenummer=&gbnummer=${gbnummer}&bezeichnung=&erstellungsdatum=&mutationsdatum=`,
+      standorteGbnummer
+    );
     cy.visit("/");
     cy.get("div[name=home-container]").should("not.contain", "Suchresultate");
-    cy.get("input[name=gbnummer]")
-      .should("be.visible")
-      .click({ force: true })
-      .type("h5r0wdwsz6ef39zb2d31a0zfou7i4tdguvddcklb");
+    cy.get("input[name=gbnummer]").should("be.visible").click({ force: true }).type(gbnummer);
     cy.get("button[name=submit-button]").should("be.visible").click();
     cy.get("div[name=home-container]").should("contain", "Suchresultate");
-    cy.get("tbody").children().should("have.length", 1);
+    cy.get("tbody").children().should("have.length", 16);
   });
 
   it("Show search result box if search for bezeichnung", function () {
+    cy.intercept(
+      `/standort?gemeindenummer=&gbnummer=&bezeichnung=Rustic%20Wooden%20Keyboard&erstellungsdatum=&mutationsdatum=`,
+      standorteBezeichnung
+    );
     cy.visit("/");
     cy.get("div[name=home-container]").should("not.contain", "Suchresultate");
     cy.get("input[name=bezeichnung]").should("be.visible").click({ force: true }).type("Rustic Wooden Keyboard");
     cy.get("button[name=submit-button]").should("be.visible").click();
     cy.get("div[name=home-container]").should("contain", "Suchresultate");
-    cy.get("tbody").children().should("have.length", 1);
+    cy.get("tbody").children().should("have.length", 21);
   });
 });
