b8aba5ee4d03583c619311aaf3b6161c47dc188b
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-07-05 09:07:21 +0200

    Fix expectation

--- src/ClientApp/cypress/integration/layout.spec.js
+++ src/ClientApp/cypress/integration/layout.spec.js
@@ -9,10 +9,6 @@ describe("General app tests", () => {
 
     cy.visit("/");
     cy.get('a[href*="/export"]').click();
-    cy.readFile("cypress\\Downloads\\data_export.csv", "utf8")
-      .should("exist")
-      .then((content) => {
-        content.should("have.text", "Angelo Kägi");
-      });
+    cy.readFile("cypress\\Downloads\\data_export.csv", "utf8").should("exist").should("contains", "Angelo Kägi");
   });
 });
