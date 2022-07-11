26b14ceb8356c589248a96a036909e946c6c7675
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-07-11 15:13:34 +0200

    Remove unused code

--- src/ClientApp/src/components/pages/Home.js
+++ src/ClientApp/src/components/pages/Home.js
@@ -137,8 +137,6 @@ export function Home() {
     <Box
       component="main"
       sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
         flexGrow: 1,
         height: "100vh",
         overflow: "auto",
