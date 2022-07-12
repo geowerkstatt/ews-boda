d69cd3da13b6d601360d02a7fe5b45e5b1e1976c
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-07-12 09:01:43 +0200

    Add tests for cascade delete

---
+++ src/Controllers/BohrprofilController.cs
@@ -0,0 +1,14 @@
+﻿using EWS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EWS;
+
+[ApiController]
+[Route("[controller]")]
+public class BohrprofilController : EwsControllerBase<Bohrprofil>
+{
+    public BohrprofilController(EwsContext context)
+        : base(context)
+    {
+    }
+}
