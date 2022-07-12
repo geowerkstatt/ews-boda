4b95ad97a22c0047c66675310edafddd587bed03
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-07-12 10:11:38 +0200

    Add user table (#74)
    * Register ews db server in pgadmin
    
    in order to improve development workflow when applying changes to the database.
    
    * Add user database table
    
    * Improve code documentation
    
    * Documentation text should end with a period

---
+++ db/05-user-table.sql
@@ -0,0 +1,26 @@
+﻿-- Add user table
+
+\connect ews
+
+DROP TABLE IF EXISTS bohrung."user";
+
+CREATE TABLE bohrung.user (
+    user_id integer NOT NULL,
+    user_name text NOT NULL,
+    user_role integer NOT NULL,
+    new_date timestamp without time zone DEFAULT now() NOT NULL,
+    mut_date timestamp without time zone,
+    new_usr character varying DEFAULT "current_user"() NOT NULL,
+    mut_usr character varying
+);
+
+CREATE SEQUENCE bohrung.user_user_id_seq
+    START WITH 100
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE bohrung.user_user_id_seq OWNED BY bohrung.user.user_id;
+ALTER TABLE ONLY bohrung.user ALTER COLUMN user_id SET DEFAULT nextval('bohrung.user_user_id_seq'::regclass);
+ALTER TABLE ONLY bohrung.user ADD CONSTRAINT pkey_user_user_id PRIMARY KEY (user_id);
---
+++ src/Models/UserRole.cs
@@ -0,0 +1,40 @@
+﻿namespace EWS.Models
+{
+    /// <summary>
+    /// Repräsentiert eine Benutzer-Rolle.
+    /// </summary>
+    public enum UserRole
+    {
+        /*
+         * Wichtig:
+         * Da in der Datenbank nur die Ids (int) der Benutzer-Rollen gespeichert werden,
+         * müssen die Werte für die Rollen stabil bleiben. Allfällige neue Rollen
+         * erhalten neue Ids.
+         */
+
+        /// <summary>
+        /// Standard Benutzer-Rolle.
+        /// Benutzer mit der Rolle <see cref="Extern"/> dürfen Standorte erstellen,
+        /// editieren und löschen solange diese noch nicht freigegeben sind.
+        /// </summary>
+        /// <seealso cref="Standort"/>
+        Extern = 1000,
+
+        /// <summary>
+        /// Sachberarbeiter AfU.
+        /// Zusätzlich zur Rolle <see cref="Extern"/> dürfen Benutzer mit der Rolle
+        /// <see cref="SachbearbeiterAfU"/> Standorte freigeben und freigegebene Standorte löschen.
+        /// </summary>
+        /// <seealso cref="Standort"/>
+        SachbearbeiterAfU = 1001,
+
+        /// <summary>
+        /// Administrator.
+        /// Zusätzlich zur Rolle <see cref="SachbearbeiterAfU"/> dürfen Benutzer mit der Rolle
+        /// <see cref="Administrator"/> Benutzer beliebigen Rollen zuweisen.
+        /// </summary>
+        /// <seealso cref="User"/>
+        /// <seealso cref="UserRole"/>
+        Administrator = 0,
+    }
+}
