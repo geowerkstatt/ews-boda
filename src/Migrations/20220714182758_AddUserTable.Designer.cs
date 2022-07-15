7d88fe5ae3b873cc94988ec4c1a57fee1e122a07
Oliver Gut <oliver.gut@geowerkstatt.ch>
2022-07-15 09:50:33 +0200

    Add migration for user table (#83)
    * Update auto-generated context model snapshot
    
    * Use EF migrations to add user table

--- src/Migrations/20220714182758_AddUserTable.Designer.cs
+++ src/Migrations/20220714182758_AddUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EWS;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EWS.Migrations
 {
     [DbContext(typeof(EwsContext))]
-    partial class EwsContextModelSnapshot : ModelSnapshot
+    [Migration("20220714182758_AddUserTable")]
+    partial class AddUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
