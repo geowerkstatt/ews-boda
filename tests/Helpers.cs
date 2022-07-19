a843001d1cabbf20313c071a7b0c569edcea873e
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2022-07-19 16:46:11 +0200

    Simpify code

--- tests/Helpers.cs
+++ tests/Helpers.cs
@@ -1,30 +1,27 @@
 ﻿using EWS.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EWS
+namespace EWS;
+
+/// <summary>
+/// Test helper methods.
+/// </summary>
+internal static class Helpers
 {
     /// <summary>
-    /// Test helpers methods.
+    /// Gets a <see cref="ControllerContext"/> with pre-configured<see cref="ClaimTypes.NameIdentifier"/> and
+    /// <see cref="ClaimTypes.Role"/> claims.
     /// </summary>
-    internal static class Helpers
+    internal static ControllerContext GetControllerContext(UserRole userRole = UserRole.Administrator)
     {
-        /// <summary>
-        /// Gets a <see cref="ControllerContext"/> with pre-configured <see cref="ClaimTypes.NameIdentifier"/> and
-        /// <see cref="ClaimTypes.Role"/> claims/>.
-        /// </summary>
-        internal static ControllerContext GetControllerContext(UserRole userRole = UserRole.Administrator)
-        {
-            var httpContext = new DefaultHttpContext();
-            (httpContext.User.Identity as ClaimsIdentity).AddClaim(new Claim(ClaimTypes.NameIdentifier, "PEEVEDSOUFFLE"));
-            (httpContext.User.Identity as ClaimsIdentity).AddClaim(new Claim(ClaimTypes.Role, userRole.ToString()));
-            return new ControllerContext() { HttpContext = httpContext };
-        }
+        var httpContext = new DefaultHttpContext();
+        var identity = (ClaimsIdentity)httpContext.User.Identity!;
+
+        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "PEEVEDSOUFFLE"));
+        identity.AddClaim(new Claim(ClaimTypes.Role, userRole.ToString()));
+
+        return new() { HttpContext = httpContext };
     }
 }
