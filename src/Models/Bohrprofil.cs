db5d3a2d853fa01dbd10a3d6a09e9309f6ab2c76
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-07-26 12:27:31 +0200

    Update data models and controllers

--- src/Models/Bohrprofil.cs
+++ src/Models/Bohrprofil.cs
@@ -50,19 +50,37 @@ namespace EWS.Models
         /// Klassierung Tektonik.
         /// </summary>
         [Column("tektonik")]
-        public int? Tektonik { get; set; }
+        public int? TektonikId { get; set; }
+
+        /// <summary>
+        /// Codetyp Klassierung der Tektonik.
+        /// </summary>
+        [ForeignKey("TektonikId")]
+        public Code? Tektonik { get; set; }
 
         /// <summary>
         /// Formation Fels.
         /// </summary>
         [Column("fmfelso")]
-        public int? FormationFels { get; set; }
+        public int? FormationFelsId { get; set; }
+
+        /// <summary>
+        /// Codetyp Klassierung der Formation Fels.
+        /// </summary>
+        [ForeignKey("FormationFelsId")]
+        public Code? FormationFels { get; set; }
 
         /// <summary>
         /// Formation Endtiefe.
         /// </summary>
         [Column("fmeto")]
-        public int? FormationEndtiefe { get; set; }
+        public int? FormationEndtiefeId { get; set; }
+
+        /// <summary>
+        /// Codetyp Klassierung der Formation Endtiefe.
+        /// </summary>
+        [ForeignKey("FormationEndtiefeId")]
+        public Code? FormationEndtiefe { get; set; }
 
         /// <summary>
         /// QualitätcodeId des Bohrprofils.
@@ -73,7 +91,7 @@ namespace EWS.Models
         /// <summary>
         /// Qualitätscode des Bohrprofils.
         /// </summary>
-        [Column("quali")]
+        [ForeignKey("QualitaetId")]
         public Code? Qualitaet { get; set; }
 
         /// <summary>
@@ -83,57 +101,37 @@ namespace EWS.Models
         public string? QualitaetBemerkung { get; set; }
 
         /// <summary>
-        /// Foreign Key: ID des Codetyps für Feld quali.
-        /// </summary>
-        [Column("h_quali")]
-        public int HQualitaetId { get; set; }
-
-        /// <summary>
         /// Codetyps für Feld quali.
         /// </summary>
-        public Code HQualitaet { get; set; }
-
-        /// <summary>
-        /// Foreign Key: ID des Codetyps für Feld tektonik.
-        /// </summary>
-        [Column("h_tektonik")]
-        public int HTektonikId { get; set; }
+        [Column("h_quali")]
+        public int HQualitaet { get; set; }
 
         /// <summary>
         /// Codetyps für Feld tektonik.
         /// </summary>
-        public CodeTyp HTektonik { get; set; }
-
-        /// <summary>
-        /// Foreign Key: ID des Codetyps für Feld fmfelso.
-        /// </summary>
-        [Column("h_fmfelso")]
-        public int HFormationFelsId { get; set; }
+        [Column("h_tektonik")]
+        public int HTektonik { get; set; }
 
         /// <summary>
         /// Codetyp für Feld fmfelso.
         /// </summary>
-        public CodeTyp HFormationFels { get; set; }
-
-        /// <summary>
-        /// Foreign Key: ID des Codetyps für Feld fmeto.
-        /// </summary>
-        [Column("h_fmeto")]
-        public int HFormationEndtiefeId { get; set; }
+        [Column("h_fmfelso")]
+        public int HFormationFels { get; set; }
 
         /// <summary>
         /// Codetyp für Feld fmeto.
         /// </summary>
-        public CodeTyp HFormationEndtiefe { get; set; }
+        [Column("h_fmeto")]
+        public int HFormationEndtiefe { get; set; }
 
         /// <summary>
         /// Schichten die dem Bohrprofil zugeordnet sind.
         /// </summary>
-        public List<Schicht> Schichten { get; set; }
+        public List<Schicht>? Schichten { get; set; }
 
         /// <summary>
         /// Vorkommnisse die dem Bohrprofil zugeordnet sind.
         /// </summary>
-        public List<Vorkommnis> Vorkomnisse { get; set; }
+        public List<Vorkommnis>? Vorkommnisse { get; set; }
     }
 }
