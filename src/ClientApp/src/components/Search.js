0ea17e875b4234c21f9d73edc926d39bafb5f72b
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-07-28 10:05:56 +0200

    Remove console.log

--- src/ClientApp/src/components/Search.js
+++ src/ClientApp/src/components/Search.js
@@ -18,7 +18,6 @@ export default function Search(props) {
     query += `&bezeichnung=${encodeURIComponent(formData.bezeichnung)}`;
     query += `&erstellungsdatum=${formData.erstellungsDatum ? new Date(formData.erstellungsDatum).toDateString() : ""}`;
     query += `&mutationsdatum=${formData.mutationsDatum ? new Date(formData.mutationsDatum).toDateString() : ""}`;
-    console.log(query);
     getStandorte(query);
   };
 
