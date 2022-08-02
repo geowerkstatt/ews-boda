fae03bb1508dcf237e269ae73422d777b4a3897a
MiraGeowerktstatt <mira.bleuler@geowerkstatt.ch>
2022-08-02 16:16:45 +0200

    Make snackbar permanent

--- src/ClientApp/src/components/SnackbarMessage.js
+++ src/ClientApp/src/components/SnackbarMessage.js
@@ -5,7 +5,11 @@ import Snackbar from "@mui/material/Snackbar";
 export default function SnackbarMessage(props) {
   const { showSuccessAlert, setShowSuccessAlert, message, variant } = props;
   return (
-    <Snackbar open={showSuccessAlert} autoHideDuration={5000} onClose={() => setShowSuccessAlert(false)}>
+    <Snackbar
+      open={showSuccessAlert}
+      ClickAwayListenerProps={{ mouseEvent: false, touchEvent: false }}
+      onClose={() => setShowSuccessAlert(false)}
+    >
       <Alert onClose={() => setShowSuccessAlert(false)} severity={variant} sx={{ width: "100%" }}>
         {message}
       </Alert>
