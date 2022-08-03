ce39a8bec14a8b3880e88192c61fd2298dcbf98c
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2022-08-03 11:38:44 +0200

    Always show all controls in Standort form

--- src/ClientApp/src/components/DateUserInputs.js
+++ src/ClientApp/src/components/DateUserInputs.js
@@ -6,8 +6,8 @@ export default function DateUserInputs(props) {
   return (
     <React.Fragment>
       <TextField
-        value={formObject.userErstellung}
-        InputLabelProps={{ shrink: formObject.userErstellung != null }}
+        value={formObject?.userErstellung}
+        InputLabelProps={{ shrink: formObject?.userErstellung != null }}
         sx={{ marginRight: "6%", width: "47%" }}
         InputProps={{
           readOnly: true,
@@ -18,8 +18,8 @@ export default function DateUserInputs(props) {
         variant="standard"
       />
       <TextField
-        value={new Date(formObject.erstellungsdatum).toLocaleDateString()}
-        InputLabelProps={{ shrink: formObject.erstellungsdatum != null }}
+        value={formObject?.erstellungsdatum ? new Date(formObject.erstellungsdatum).toLocaleDateString() : null}
+        InputLabelProps={{ shrink: formObject?.erstellungsdatum != null }}
         InputProps={{
           readOnly: true,
         }}
@@ -29,35 +29,33 @@ export default function DateUserInputs(props) {
         type="text"
         variant="standard"
       />
-      {formObject.userMutation && (
-        <React.Fragment>
-          <TextField
-            value={formObject.userMutation}
-            InputLabelProps={{ shrink: formObject.userMutation != null }}
-            sx={{ marginRight: "6%", width: "47%" }}
-            InputProps={{
-              readOnly: true,
-            }}
-            margin="normal"
-            label="Zuletzt geändert durch"
-            type="text"
-            variant="standard"
-          />
-          <TextField
-            name="mutationsdatum"
-            value={formObject.mutationsdatum ? new Date(formObject.mutationsdatum).toLocaleDateString() : null}
-            InputLabelProps={{ shrink: formObject.mutationsdatum != null }}
-            InputProps={{
-              readOnly: true,
-            }}
-            sx={{ width: "47%" }}
-            margin="normal"
-            label="Zuletzt geändert am"
-            type="text"
-            variant="standard"
-          />
-        </React.Fragment>
-      )}
+      <React.Fragment>
+        <TextField
+          value={formObject?.userMutation}
+          InputLabelProps={{ shrink: formObject?.userMutation != null }}
+          sx={{ marginRight: "6%", width: "47%" }}
+          InputProps={{
+            readOnly: true,
+          }}
+          margin="normal"
+          label="Zuletzt geändert durch"
+          type="text"
+          variant="standard"
+        />
+        <TextField
+          name="mutationsdatum"
+          value={formObject?.mutationsdatum ? new Date(formObject.mutationsdatum).toLocaleDateString() : null}
+          InputLabelProps={{ shrink: formObject?.mutationsdatum != null }}
+          InputProps={{
+            readOnly: true,
+          }}
+          sx={{ width: "47%" }}
+          margin="normal"
+          label="Zuletzt geändert am"
+          type="text"
+          variant="standard"
+        />
+      </React.Fragment>
     </React.Fragment>
   );
 }
