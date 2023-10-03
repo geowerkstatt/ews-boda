949e5ddbdd4cd6bda6884433f726c9fbb5fb27ec
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-10-03 16:15:43 +0200

    Add DataService API JSON mock responses

--- tests/DataServiceControllerTest.cs
+++ tests/DataServiceControllerTest.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using NetTopologySuite.Geometries;
@@ -19,8 +18,53 @@ public class DataServiceTest
     [TestInitialize]
     public void Initialize()
     {
+        var mockHttp = new MockHttpMessageHandler();
+
+        // Gemeinde Langendorf
+        mockHttp
+            .When("*ch.so.agi.gemeindegrenzen.data/?bbox=2605532,1229554,2605532,1229554")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfJson);
+
+        mockHttp
+            .When("*ch.so.agi.gemeindegrenzen.data/?bbox=2605531,1229553,2605531,1229553")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfJson);
+
+        mockHttp
+            .When("*ch.so.agi.gemeindegrenzen.data/?bbox=2605591,1229592,2605591,1229592")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfJson);
+
+        mockHttp
+            .When("*ch.so.agi.gemeindegrenzen.data/?bbox=2605590,1229590,2605590,1229590")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfJson);
+
+        mockHttp
+            .When("*ch.so.agi.av.grundstuecke.rechtskraeftig.data/?bbox=2605532,1229554,2605532,1229554")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfGrundbuch1950Json);
+
+        mockHttp
+            .When("*ch.so.agi.av.grundstuecke.rechtskraeftig.data/?bbox=2605531,1229553,2605531,1229553")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfGrundbuch1950Json);
+
+        mockHttp
+            .When("*ch.so.agi.av.grundstuecke.rechtskraeftig.data/?bbox=2605591,1229592,2605591,1229592")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfGrundbuch1002Json);
+
+        mockHttp
+            .When("*ch.so.agi.av.grundstuecke.rechtskraeftig.data/?bbox=2605590,1229590,2605590,1229590")
+            .Respond("application/json", DataServiceApiResponseMocks.GemeindeLangendorfGrundbuch1002Json);
+
+        // Empty feature collection for unknown location.
+        mockHttp
+            .When("*ch.so.agi.gemeindegrenzen.data/?bbox=2147483647,-2147483648,2147483647,-2147483648")
+            .Respond("application/json", DataServiceApiResponseMocks.EmptyFeatureCollectionJson);
+
+        mockHttp
+            .When("*ch.so.agi.av.grundstuecke.rechtskraeftig.data/?bbox=2147483647,-2147483648,2147483647,-2147483648")
+            .Respond("application/json", DataServiceApiResponseMocks.EmptyFeatureCollectionJson);
+
+        var mockHttpClient = new HttpClient(mockHttp);
         var httpClientFactoryMock = new Mock<IHttpClientFactory>();
-        httpClientFactoryMock.Setup(x => x.CreateClient("DataService")).Returns(new HttpClient());
+        httpClientFactoryMock.Setup(x => x.CreateClient("DataService")).Returns(mockHttpClient);
         dataService = new DataService(httpClientFactoryMock.Object, new Mock<ILogger<DataService>>().Object);
     }
 
